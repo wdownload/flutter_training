d5244a0ae7d98e6b61b30085b876598c2c6b1a48
Oldemiro Americo Baloi <oldemiroamerico@gmail.com>
2021-03-21 14:20:26 +0200

    using custom widget

--- puzzle/lib/main.dart
+++ puzzle/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:puzzle/question.dart';
 
 void main() => runApp(MyApp());
 
@@ -31,7 +32,7 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
+            Question(questions[questionIndex]),
             ElevatedButton(child: Text("Answer 1"), onPressed: answerQuestion),
             ElevatedButton(
                 child: Text("Answer 2"),
